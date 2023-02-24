aa48265ccff13684e62f31cfb3dc22a553538c34
JosePerdomo1 <125402882+JosePerdomo1@users.noreply.github.com>
2023-02-24 12:21:25 +0100

    plantilla proyecto
    solo tiene el hello world

---
+++ JoseJohn/src/main/java/es/iesvigan/jose/josejohn/JoseJohn.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package es.iesvigan.jose.josejohn;
+
+/**
+ *
+ * @author Jose Antonio
+ */
+public class JoseJohn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
