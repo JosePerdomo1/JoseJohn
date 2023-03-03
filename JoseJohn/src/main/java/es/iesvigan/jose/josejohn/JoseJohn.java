2b877880f397a43470ef1ce41d300b902b7643d4
JosePerdomo1 <125402882+JosePerdomo1@users.noreply.github.com>
2023-03-03 22:57:38 +0000

    falta el connect 4 y los colores

--- JoseJohn/src/main/java/es/iesvigan/jose/josejohn/JoseJohn.java
+++ JoseJohn/src/main/java/es/iesvigan/jose/josejohn/JoseJohn.java
@@ -12,28 +12,129 @@ import java.util.Random;
  * @author Jose Antonio
  */
 public class JoseJohn {
-    
-    public static int Elegir_inicio(){
-        Random random = new Random();		
-        return random.nextInt(2) + 1;
+    private static int jugadorActual = 0; // Se establece una variable para el jugador actual
+
+    public static int Elegir_inicio() {
+        Random random = new Random();
+        int jugador_actual = random.nextInt(2) + 1;
+        return jugador_actual;
     }
-    public static char Mostrar_castilla_usuario(int numeroCasilla){
-        if (numeroCasilla == 0){
-            return '-';
-        }else if (numeroCasilla == 1){
-            return 'X';
-        }else if (numeroCasilla == 2){
-            return 'O';
+
+    // Función para alternar los turnos
+    public static int cambiarJugador() {
+        if (jugadorActual == 0) {
+            jugadorActual = Elegir_inicio(); // Se genera un valor aleatorio para el primer turno
+        } else {
+            // Se alterna entre los dos jugadores
+            if (jugadorActual == 1) {
+                return jugadorActual = 2;
+            } else {
+                return jugadorActual = 1;
+            }
         }
-        return 0;
+        return jugadorActual;
+
     }
+
+    public static final int NumerosFila = 6;
+    public static final int NumerosColumna = 7;
+
+    public static boolean verificarGanador(char[][] mostrar_tablero, int fila, int columna) {
+        char valor = mostrar_tablero[fila][columna];
+        int contador = 0;
     
-   
+        // Verificar en vertical
+        for (int i = 0; i < 6 && contador < 4; i++) {
+            if (mostrar_tablero[i][columna] == valor) {
+                contador += 1;
+            } else {
+                contador = 0;
+            }
+        }
+        if (contador >= 4) {
+            return true;
+        }
+    
+        // Verificar en horizontal
+        contador = 0;
+        for (int j = 0; j < 7 && contador < 4; j++) {
+            if (mostrar_tablero[fila][j] == valor) {
+                contador += 1;
+            } else {
+                contador = 0;
+            }
+        }
+        if (contador >= 4) {
+            return true;
+        }
+    
+        // Verificar en diagonal (desde la posición actual hacia arriba y hacia la izquierda)
+        contador = 0;
+        int i = fila, j = columna;
+        while (i >= 0 && j >= 0 && mostrar_tablero[i][j] == valor) {
+            contador += 1;
+            i--;
+            j--;
+        }
+        if (contador >= 4) {
+            return true;
+        }
+    
+        // Verificar en diagonal (desde la posición actual hacia abajo y hacia la derecha)
+        contador = 0;
+        i = fila + 1;
+        j = columna + 1;
+        while (i < 6 && j < 7 && mostrar_tablero[i][j] == valor) {
+            contador += 1;
+            i++;
+            j++;
+        }
+        if (contador >= 4) {
+            return true;
+        }
+    
+        // Verificar en diagonal (desde la posición actual hacia arriba y hacia la derecha)
+        contador = 0;
+        i = fila;
+        j = columna;
+        while (i >= 0 && j < 7 && mostrar_tablero[i][j] == valor) {
+            contador += 1;
+            i--;
+            j++;
+        }
+        if (contador >= 4) {
+            return true;
+        }
+    
+        // Verificar en diagonal (desde la posición actual hacia abajo y hacia la izquierda)
+        contador = 0;
+        i = fila + 1;
+        j = columna - 1;
+        while (i < 6 && j >= 0 && mostrar_tablero[i][j] == valor) {
+            contador += 1;
+            i++;
+            j--;
+        }
+        if (contador >= 4) {
+            return true;
+        }
+    
+        // Si no hay ganador, retornar false
+        return false;
+    }
+
     public static void main(String[] args) {
-        Tablero Tablero= new Tablero();
+        Tablero Tablero = new Tablero();
         System.out.println("Este es el tablero actualmente");
-        
-        Tablero.insertarValor(System.in);
-        Tablero.imprimir();
+        // Con este for insertamos el valor "X" o "O" y se acabara cuando el tablero
+        // este lleno, (creo que es mejor que hacer una funcion de tablero lleno para
+        // que no sean tantas lineas)
+        for (int i = 0; i < 36; i++) {
+            Tablero.insertarValor(System.in);
+            Tablero.imprimir();
+            
+           
+        }
+
     }
 }
--- JoseJohn/src/main/java/es/iesvigan/jose/josejohn/Tablero.java
+++ JoseJohn/src/main/java/es/iesvigan/jose/josejohn/Tablero.java
@@ -1,11 +1,14 @@
 package es.iesvigan.jose.josejohn;
+
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.InputStream;
 
 public class Tablero {
-    private boolean decision_tablero;
-    private char[][] mostrar_tablero;
+    public char[][] mostrar_tablero;
+    public int fila = 5;
+    public int columna;
+
 
     public void imprimir() {
         for (int i = 0; i < mostrar_tablero.length; i++) {
@@ -15,33 +18,50 @@ public class Tablero {
             System.out.println();
         }
     }
+
+    // public static int[][] obtenerTablero() {
+    // int[][] tablero = new int[6][6];
+    // for (int i = 0; i < tablero.length; i++) {
+    // for (int j = 0; j < tablero[i].length; j++) {
+    // tablero[i][j] = mostrar_tablero[i][j];
+    // }
+    // }
+    // return tablero;
+    // }
+
+    public static char Mostrar_castilla_usuario(int Elegir_inicio) {
+        if (Elegir_inicio == 0) {
+            return '-';
+        } else if (Elegir_inicio == 1) {
+            return 'X';
+        } else if (Elegir_inicio == 2) {
+            return 'O';
+        } else {
+            return 0;
+        }
+
+    }
+
     public void insertarValor(InputStream entradaDatos) {
-        final Scanner scanner = new Scanner(entradaDatos);
+        Scanner scanner = new Scanner(entradaDatos);
         System.out.print("Introduce el número de columna (1-6): ");
-        int columna = scanner.nextInt() - 1;
-        
+        columna = scanner.nextInt() -1;
+
         // Buscar la primera posición libre en la columna
-        int fila = 5;
-        while (fila >= 0 && mostrar_tablero[fila][columna] != '0') {
+        while (fila >= 0 && mostrar_tablero[fila][columna] != '-') {
             fila--;
         }
-        
-        // Si se encontró una posición libre, asignar el valor
+
+        // Si se encontró una posición libre, se agina el valor
         if (fila >= 0) {
-            System.out.print("Introduce el valor para colocar en esa celda (1 o 2): ");
-            char valor = scanner.next().charAt(0);
-            if (valor != '1' && valor != '2') {
-                System.out.println("Valor inválido. Introduce 1 o 2.");
-                scanner.close();
-                return;
-            }
+            int inicio = JoseJohn.cambiarJugador();
+            char valor = Mostrar_castilla_usuario(inicio);
             mostrar_tablero[fila][columna] = valor;
         } else {
             System.out.println("La columna está llena. Introduce una columna diferente.");
             insertarValor(entradaDatos); // Llamar recursivamente a la función con los mismos datos de entrada
         }
-        
-        scanner.close();
+
     }
 
     public Tablero() {
@@ -49,13 +69,6 @@ public class Tablero {
         for (int i = 0; i < mostrar_tablero.length; i++) {
             Arrays.fill(mostrar_tablero[i], '-');
         }
-    }
-    public boolean isDecision_tablero() {
-        return decision_tablero;
-    }
-
-    public void setDecision_tablero(boolean decision_tablero) {
-        this.decision_tablero = decision_tablero;
     }
 
     public char[][] getMostrar_tablero() {
@@ -65,6 +78,21 @@ public class Tablero {
     public void setMostrar_tablero(char[][] mostrar_tablero) {
         this.mostrar_tablero = mostrar_tablero;
     }
-    
-    
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
 }
